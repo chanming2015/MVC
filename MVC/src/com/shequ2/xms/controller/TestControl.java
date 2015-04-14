4aca217aa7ff003e09e60a401fdb66d98363fe26
chanming2015 <476076812@qq.com>
2015-04-14 18:09:57 +0800

    2

--- MVC/src/com/shequ2/xms/controller/TestControl.java
+++ MVC/src/com/shequ2/xms/controller/TestControl.java
@@ -10,9 +10,11 @@ package com.shequ2.xms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.shequ2.xms.entity.User;
 import com.shequ2.xms.service.IUserService;
@@ -31,13 +33,16 @@ import com.shequ2.xms.service.IUserService;
  * Description:
  * Version:
  */
+//使用注解定义控制器
 @Controller
+//定义类拦截
 @RequestMapping("/test")
 public class TestControl {
 	
 	@Autowired
 	private IUserService userService;
 
+	//定义方法拦截
 	@RequestMapping(value="/toLogin", method=RequestMethod.GET)
 	public String toLogin(Model model){
 		return "toLogin";
@@ -52,6 +57,22 @@ public class TestControl {
 			model.addAttribute("message", "该用户不存在！");
 		}
 		return "doLogin";
+	}
+	
+	//使用URL传参
+	@RequestMapping(value="/index/{name}", method=RequestMethod.GET)
+	public String index(@PathVariable String name,@MatrixVariable(required=false, defaultValue="123") int pass, Model model){
+		model.addAttribute("name", name);
+		model.addAttribute("pass", pass);
+		return "index";
+	}
+	
+	@RequestMapping(value="/index2/{name}/{pass}", method=RequestMethod.GET, produces="application/json")
+	@ResponseBody
+	public User index2(@PathVariable String name, @PathVariable String pass, Model model){
+		User user = userService.getUser(name, pass);
+		System.out.println(user.getUsername());
+		return user;
 	}
 
 }
